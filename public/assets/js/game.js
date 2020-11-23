a4320764b457994c64c14c69a7435a231d9a96f9
Nolan <kurylon@telus.net>
2020-11-22 17:04:36 -0800

    merge lobby

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -53,43 +53,55 @@ class WorldScene extends Phaser.Scene {
     this.socket.emit("new_player", data)
     this.otherPlayers = this.physics.add.group();
 
-      this.redirect(this.socket);
-      this.endGame(this.socket);
-  
-      // create map
-      this.createMap();
 
-      
-  
-      // create player animations
-      this.createAnimations();
-      this.cursors = this.input.keyboard.createCursorKeys();
-      this.cursors = this.input.keyboard.addKeys({up:Phaser.Input.Keyboard.KeyCodes.W, down:Phaser.Input.Keyboard.KeyCodes.S, left:Phaser.Input.Keyboard.KeyCodes.A, right:Phaser.Input.Keyboard.KeyCodes.D});
+    this.redirect(this.socket);
+    this.endGame(this.socket);
 
-      
-      this.socket.on('startGame', function () {
-        setTimeout(function(){
-          gameStarted = true;
-        }, 5000)
-        
-        
-      }.bind(this))
-      // user input
-     
-      // create enemies
-      //this.createEnemies();
-      // listen for web socket events
-      this.socket.on('currentPlayers', function (players) {
-        Object.keys(players).forEach(function (id) {
-          if (players[id].playerId === this.socket.id) {
-            this.createPlayer(players[id]);
-          } else {
-            this.addOtherPlayers(players[id]);
-          }
-        }.bind(this));
-      }.bind(this));
+    // create map
+    this.createMap();
+
     
 
+    // create player animations
+    this.createAnimations();
+    
+
+    // user input
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.addKeys({up:Phaser.Input.Keyboard.KeyCodes.W, down:Phaser.Input.Keyboard.KeyCodes.S, left:Phaser.Input.Keyboard.KeyCodes.A, right:Phaser.Input.Keyboard.KeyCodes.D});
+
+    this.socket.on('startGame', function () {
+      setTimeout(function(){
+        gameStarted = true;
+      }, 5000)
+      
+      
+    }.bind(this))
+
+    // create enemies
+    //this.createEnemies();
+    // listen for web socket events
+    this.socket.on('currentPlayers', function (players) {
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === this.socket.id) {
+          this.createPlayer(players[id]);
+        } else {
+          this.addOtherPlayers(players[id]);
+        }
+      }.bind(this));
+    }.bind(this));
+  
+    this.socket.on('newPlayer', function (playerInfo) {
+      this.addOtherPlayers(playerInfo);
+    }.bind(this))
+
+    this.socket.on('disconnect', function (playerId) {
+      this.otherPlayers.getChildren().forEach(function (player) {
+        if (playerId === player.playerId) {
+          player.destroy();
+        }
+      }.bind(this));
+    }.bind(this));
 
     this.socket.on('playerMoved', function (playerInfo) {
       this.otherPlayers.getChildren().forEach(function (player) {
@@ -118,182 +130,164 @@ class WorldScene extends Phaser.Scene {
         i--;
       }
     })
-
-
-  
-  
-    this.socket.on('newPlayer', function (playerInfo) {
-      this.addOtherPlayers(playerInfo);
-    }.bind(this))
-
-    this.socket.on('disconnect', function (playerId) {
-      this.otherPlayers.getChildren().forEach(function (player) {
-        if (playerId === player.playerId) {
-          player.destroy();
-        }
-      }.bind(this));
-    }.bind(this));
-  }
-    redirect(socket){
-      socket.on('redirect', function(destination) {
-          window.location.href = destination;
-      });
-    }
-    endGame(socket){
-      socket.on('endGame', function(vars) {
-        //update database here with user + game stats and then nav to game stats page
-          window.location.href = "/post_game?game_id=" + game_id + "&user_id=" + user_id;
-      });
-    }
-  
-    createMap() {
-      // create the map
-      this.map = this.make.tilemap({
-        key: 'map'
-      });
-  
-      // first parameter is the name of the tilemap in tiled
-      var tiles = this.map.addTilesetImage('spritesheet', 'tiles', 16, 16, 1, 2);
-  
-      // creating the layers
-      this.map.createStaticLayer('Grass', tiles, 0, 0);
-      this.map.createStaticLayer('Obstacles', tiles, 0, 0);
-  
-      // don't go out of the map
-      this.physics.world.bounds.width = this.map.widthInPixels;
-      this.physics.world.bounds.height = this.map.heightInPixels;
-    }
-  
-    createAnimations() {
-      //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
-      this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('player', {
-          frames: [1, 7, 1, 13]
-        }),
-        frameRate: 10,
-        repeat: -1
-      });
-  
-      // animation with key 'right'
-      this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('player', {
-          frames: [1, 7, 1, 13]
-        }),
-        frameRate: 10,
-        repeat: -1
-      });
-  
-      this.anims.create({
-        key: 'up',
-        frames: this.anims.generateFrameNumbers('player', {
-          frames: [2, 8, 2, 14]
-        }),
-        frameRate: 10,
-        repeat: -1
-      });
-  
-      this.anims.create({
-        key: 'down',
-        frames: this.anims.generateFrameNumbers('player', {
-          frames: [0, 6, 0, 12]
-        }),
-        frameRate: 10,
-        repeat: -1
-      });
-    }
-  
-    createPlayer(playerInfo) {
-      // our player sprite created through the physics system
-      this.player = this.add.sprite(0, 0, 'player', 6);
-      if(playerInfo.team == 'A'){
-        this.player.setTint(0x0000FF);
-      }
-      else{
-        this.player.setTint(0xFF0000);
-      }
-  
-      this.container = this.add.container(playerInfo.x, playerInfo.y);
-      this.container.setSize(16, 16);
-      this.physics.world.enable(this.container);
-      this.container.add(this.player);
-  
-      // update camera
-      this.updateCamera();
-  
-      // don't go out of the map
-      this.container.body.setCollideWorldBounds(true);
-  
-      this.physics.add.collider(this.container, this.spawns);
-    }
-  
-    addOtherPlayers(playerInfo) {
-      const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player', 9);
-      if(playerInfo.team == 'A'){
-        otherPlayer.setTint(0x0000FF);
-      }
-      else{
-        otherPlayer.setTint(0xFF0000);
-      }
-      
-      otherPlayer.playerId = playerInfo.playerId;
-      this.otherPlayers.add(otherPlayer);
-    }
-  
-    updateCamera() {
-      // limit camera to map
-      this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-      this.cameras.main.startFollow(this.container);
-      this.cameras.main.roundPixels = true; // avoid tile bleed
-    }
-  
-    createEnemies() {
-      // where the enemies will be
-      this.spawns = this.physics.add.group({
-        classType: Phaser.GameObjects.Sprite
-      });
-      for (var i = 0; i < 20; i++) {
-        const location = this.getValidLocation();
-        // parameters are x, y, width, height
-        var enemy = this.spawns.create(location.x, location.y, this.getEnemySprite());
-        enemy.body.setCollideWorldBounds(true);
-        enemy.body.setImmovable();
-      }
-    }
-  
-    getEnemySprite() {
-      var sprites = ['golem', 'ent', 'demon', 'worm', 'wolf'];
-      return sprites[Math.floor(Math.random() * sprites.length)];
-    }
-  
-    getValidLocation() {
-      var validLocation = false;
-      var x, y;
-      while (!validLocation) {
-        x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
-        y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
-  
-        var occupied = false;
-        this.spawns.getChildren().forEach((child) => {
-          if (child.getBounds().contains(x, y)) {
-            occupied = true;
-          }
-        });
-        if (!occupied) validLocation = true;
-      }
-      return { x, y };
-    }
-  
-    onMeetEnemy(player, zone) {
-      // we move the zone to some other location
-      zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
-      zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
-    }
-       
-
     
+  }
+
+  redirect(socket){
+    socket.on('redirect', function(destination) {
+        window.location.href = destination;
+    });
+  }
+
+  endGame(socket){
+    socket.on('endGame', function(vars) {
+      //update database here with user + game stats and then nav to game stats page
+        window.location.href = "/post_game?game_id=" + game_id + "&user_id=" + user_id;
+    });
+  }
+
+  createMap() {
+    // create the map
+    this.map = this.make.tilemap({
+      key: 'map'
+    });
+
+    // first parameter is the name of the tilemap in tiled
+    let tiles = this.map.addTilesetImage('spritesheet', 'tiles', 16, 16, 1, 2);
+
+    // creating the layers
+    this.map.createStaticLayer('Grass', tiles, 0, 0);
+    this.map.createStaticLayer('Obstacles', tiles, 0, 0);
+
+    // don't go out of the map
+    this.physics.world.bounds.width = this.map.widthInPixels;
+    this.physics.world.bounds.height = this.map.heightInPixels;
+  }
+
+  createAnimations() {
+    //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [1, 7, 1, 13]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // animation with key 'right'
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [1, 7, 1, 13]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [2, 8, 2, 14]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [0, 6, 0, 12]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+  }
+
+ 
+createPlayer(playerInfo) {
+  // our player sprite created through the physics system
+  this.player = this.add.sprite(0, 0, 'player', 6);
+  if(playerInfo.team == 'A'){
+    this.player.setTint(0x0000FF);
+  }
+  else{
+    this.player.setTint(0xFF0000);
+  }
+
+  this.container = this.add.container(playerInfo.x, playerInfo.y);
+  this.container.setSize(16, 16);
+  this.physics.world.enable(this.container);
+  this.container.add(this.player);
+
+  // update camera
+  this.updateCamera();
+
+  // don't go out of the map
+  this.container.body.setCollideWorldBounds(true);
+
+  this.physics.add.collider(this.container, this.spawns);
+}
+
+
+addOtherPlayers(playerInfo) {
+  const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player', 9);
+  if(playerInfo.team == 'A'){
+    otherPlayer.setTint(0x0000FF);
+  }
+  else{
+    otherPlayer.setTint(0xFF0000);
+  }
   
+  otherPlayer.playerId = playerInfo.playerId;
+  this.otherPlayers.add(otherPlayer);
+}
+
+
+  updateCamera() {
+    // limit camera to map
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.cameras.main.startFollow(this.container);
+    this.cameras.main.roundPixels = true; // avoid tile bleed
+    
+  }
+
+  createEnemies() {
+    // where the enemies will be
+    this.spawns = this.physics.add.group({
+      classType: Phaser.GameObjects.Sprite
+    });
+    for (let i = 0; i < 20; i++) {
+      const location = this.getValidLocation();
+      // parameters are x, y, width, height
+      let enemy = this.spawns.create(location.x, location.y, this.getEnemySprite());
+      enemy.body.setCollideWorldBounds(true);
+      enemy.body.setImmovable();
+    }
+  }
+
+  getEnemySprite() {
+    let sprites = ['golem', 'ent', 'demon', 'worm', 'wolf'];
+    return sprites[Math.floor(Math.random() * sprites.length)];
+  }
+
+  getValidLocation() {
+    let validLocation = false;
+    let x, y;
+    while (!validLocation) {
+      x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+      y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+
+      let occupied = false;
+      this.spawns.getChildren().forEach((child) => {
+        if (child.getBounds().contains(x, y)) {
+          occupied = true;
+        }
+      });
+      if (!occupied) validLocation = true;
+    }
+    return { x, y };
+  }
 
   onMeetEnemy(player, zone) {
     // we move the zone to some other location
@@ -303,7 +297,7 @@ class WorldScene extends Phaser.Scene {
 
 
   update() {
-    if (this.container) {
+    if (this.container && gameStarted) {
       this.container.body.setVelocity(0);
 
       // shooting
