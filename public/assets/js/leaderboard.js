b863877c192e4115bd51d1d0b389f3f86a313542
Nolan <kurylon@telus.net>
2020-11-23 22:04:00 -0800

    leaderboard UI done

--- public/assets/js/leaderboard.js
+++ public/assets/js/leaderboard.js
@@ -1,48 +1,38 @@
-let red_killcount = 0;
-let blue_killcount = 0;
-let red_text;
-let blue_text;
+let teamA_killcount = 0;
+let teamB_killcount = 0;
+let teamA_text;
+let teamB_text;
 
+const kill_multiplier = 10;
 
-
-
- class LeaderBoard extends Phaser.Scene {
-    constructor() {
-      super({
-        key: 'LeaderBoard',
-        active: true
-      });
-    }
-    
-    create() {
-
-
-      
-        this.socket = io();
-
-
-        
-        red_text = this.add.text(175, 0, 'Red Team Kills: '+red_killcount.toString(), { fontFamily: 'Arial', fontSize: '14px' }).setScrollFactor(0,0);
-        blue_text = this.add.text(175, 12, 'Blue Team Kills: '+blue_killcount.toString(), { fontFamily: 'Arial', fontSize: '14px' }).setScrollFactor(0,0);
-        red_text.setResolution(10);
-        blue_text.setResolution(10);
-
-        red_text.setText("Red Team Kills: "+red_killcount.toString());
-        blue_text.setText("Blue Team Kills: "+blue_killcount.toString());
-
-
-        this.socket.on('updateLeaderboard', function(playerInfo) {
-            if (playerInfo.shooter.team === "A") {
-                blue_killcount++;
-            } else if (playerInfo.shooter.team === "B") {
-            red_killcount++;
-            }
-            red_text.setText("Red Team Kills: "+red_killcount.toString());
-      blue_text.setText("Blue Team Kills: "+blue_killcount.toString());
-        }.bind(this));
-
-    }
-  
+class LeaderBoard extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'LeaderBoard',
+      active: true
+    });
   }
+  
+  create() {
 
-  export default LeaderBoard
+    this.socket = io();
+
+    teamA_text = this.add.text(5, 5, 'Team A Score: 0', { fontFamily: 'Arial', fontSize: '14px', color:'#0000FF' }).setScrollFactor(0,0);
+    teamB_text = this.add.text(195, 5, 'Team B Score: 0', { fontFamily: 'Arial', fontSize: '14px', color:'#FF0000' }).setScrollFactor(0,0);
+    teamA_text.setResolution(10);
+    teamB_text.setResolution(10);
+
+    this.socket.on('updateLeaderboard', function(playerInfo) {
+        if (playerInfo.shooter.team === "A") {
+          teamA_killcount++;
+        } 
+        else if (playerInfo.shooter.team === "B") {
+          teamB_killcount++;
+        }
+        teamA_text.setText("Team A Score: " + (teamA_killcount * kill_multiplier).toString());
+        teamB_text.setText("Team B Score: "+ (teamB_killcount * kill_multiplier).toString());
+    }.bind(this));
+  }
+}
+
+export default LeaderBoard
